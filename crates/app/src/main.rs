0f5a64245d6418b73974018c7f464460bfc16750
arshGoyalDev <arshgoyalDev@gmail.com>
2026-05-10 17:48:58 +0530

    add: custom window title bar

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,10 +1,16 @@
 mod state;
+mod tab;
 
+use iced::window;
 use state::Browser;
 
 fn main() -> iced::Result {
   iced::application("project-agr", Browser::update, Browser::view)
     .subscription(Browser::subscription)
     .theme(Browser::theme)
+    .window(window::Settings {
+      decorations: false,
+      ..Default::default()
+    })
     .run_with(Browser::new)
 }
